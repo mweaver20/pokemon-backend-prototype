824e426db0336b29c86d440603509a8ab29ce430
manderson724 <90173220+manderson724@users.noreply.github.com>
2023-10-22 20:14:00 -0400

    Add files via upload

---
+++ gameProgress.java
@@ -0,0 +1,18 @@
+
+public class gameProgress {
+	static void progressService() {
+		System.out.println("A player’s score, health, status, and current level will be tracked in real-time "
+				+ "and stored in a temporary location for access throughout the game as long as the progress "
+				+ "has been saved. A player’s progress is cumulative while the game is in progress. "
+				+ "If a player ends the game without saving the current character’s status, the progress and "
+				+ "all other data are lost and the game will reset to level one once restarted. If a player "
+				+ "saves their current status, then that player can resume from the last known stage in the game.");
+	}
+
+
+	public static void main(String[] args) {
+		progressService(); // TODO Auto-generated method stub
+
+	}
+
+}
